145d6f2e2a8b07c542d479d0a3191254ac5dbc40
lvovmaxim <160147521+bal-tea-ka@users.noreply.github.com>
2024-03-01 14:18:25 +0300

    Update Retailitem.cpp

--- OOP C++/Retailitem.cpp
+++ OOP C++/Retailitem.cpp
@@ -21,15 +21,15 @@ int main() {
 	setlocale(LC_ALL, "RUS");
 
 	Retailitem a[3];
-	a[0].set_description("Пиджак");
+	a[0].set_description("РџРёРґР¶Р°Рє");
 	a[0].set_count(12);
 	a[0].set_price(59.95);
 
-	a[1].set_description("Дизайнерские джинсы");
+	a[1].set_description("Р”РёР·Р°Р№РЅРµСЂСЃРєРёРµ РґР¶РёРЅСЃС‹");
 	a[1].set_count(40);
 	a[1].set_price(34.95);
 	
-	a[2].set_description("Рубашка");
+	a[2].set_description("Р СѓР±Р°С€РєР°");
 	a[2].set_count(20);
 	a[2].set_price(24.95);
 
